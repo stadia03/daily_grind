4a4654f1002a90f8a9c4f5c8bc5aa014bf855dbb
stadia03 <gaurab.working@gmail.com>
2024-02-02 22:02:18 +0530

    contains_duplicate

--- Contains_Duplicate.cpp
+++ Contains_Duplicate.cpp
@@ -1,20 +1,25 @@
 //Contains_Duplicate
 
 #include <bits/stdc++.h>
-
-
 using namespace std;
  
 bool containsDuplicate(vector<int>& nums) {
+    unordered_set <int> seen ;
         for (int i = 0; i < nums.size(); i++) {
-            cout << nums[i];
+            
+            if(seen.find(nums[i])!= seen.end())
+                  return true;
+            else
+              seen.insert(nums[i]);
         }
-        return true;
+        // auto it = seen.find(1);
+        // cout<< *it;
+        return false;
     }
 
 int main() {
  
-  vector <int> nums={1,2,3,1};
+  vector <int> nums={1,2,3,5,67,8,6,5,5};
   cout<<containsDuplicate(nums);
  
   return 0;
