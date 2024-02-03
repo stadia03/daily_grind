df4e2b84d4955b872af95db1c05f94e387527402
stadia03 <gaurab.working@gmail.com>
2024-02-03 20:50:26 +0530

    newstuff

---
+++ valid-anagram.cpp
@@ -0,0 +1,27 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ bool isAnagram(string s, string t) {
+     
+        sort(s.begin(),s.end());
+        
+        sort(t.begin(),t.end());
+           if(s==t)
+          return  true;
+
+
+        return false;
+    }
+
+
+int main() {
+ 
+  string s,t;
+  cin>>s>>t;
+  cout<<isAnagram(s,t);
+ 
+  return 0;
+}
--- valid-palindrome.cpp
+++ valid-palindrome.cpp
@@ -1,9 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
  
+
+ bool isPalindrome(string str) {
+        string nstr;
+        for(int i=0;i<str.length();i++){
+          if(str[i]>=97 &&  str[i]<=122 || str[i]>=65 &&  str[i]<=90)
+            nstr.push_back(str[i]);
+    }
+      // if(nstr.length()==1 && str.length()!=1)
+     // return 0;
+    for(int i=0;i<nstr.length()/2;i++){
+      if(tolower(nstr[i])!=tolower(nstr[nstr.length()-i-1])){
+        // cout<<"str["<<i<<"] = "<<str[i]<<" str[str.length()-i-1]="<<str[str.length()-i-1]<<" ";
+        //  cout<<str[2]; 
+          return 0;
+      }
+        
+
+    }
+    
+    return 1;
+    }
+
 int main() {
  
-  
+  string str;
+  getline(cin,str);
+  cout<<isPalindrome(str);
  
   return 0;
 }
