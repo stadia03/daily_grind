e10015c4c2f38a46383bf11555ebc861f3a41d3a
stadia03 <gaurab.working@gmail.com>
2024-03-04 23:33:17 +0530

    bubbleblaa

---
+++ bubble_sort.cpp
@@ -0,0 +1,46 @@
+//bubble sort
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void bubble_sort(int arr[], int n, int  swaps) {
+
+  for(int i=0;i<n-1;i++){
+    // cout<<endl<<"i="<<i<<endl;
+    for(int j=0;j<n-1;j++){
+      if(swaps){
+        if(arr[j]>arr[j+1]){
+          swap(arr[j+1], arr[j]);
+      }
+    }
+    else{
+      if(arr[i]<arr[j]){
+          swap(arr[j+1], arr[j]);
+      }
+    }
+    // for(int i=0;i<n;i++)
+    // cout<<arr[i]<<" ";
+    // cout<<" "<<arr[j]<<" "<<arr[j+1];
+    // cout<<endl;
+  }
+   
+}
+}
+
+int main() {
+ 
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++)
+    cin>>arr[i];
+
+// 1 for asc , 0 for desc
+  bubble_sort(arr,n,1);
+
+  for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+
+ 
+  return 0;
+}
