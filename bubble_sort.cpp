360264aa6e95b5ab5e857c2f03338acd808224de
stadia03 <gaurab.working@gmail.com>
2024-03-05 00:43:29 +0530

    dailyyy

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -7,23 +7,28 @@ void bubble_sort(int arr[], int n, int  swaps) {
 
   for(int i=0;i<n-1;i++){
     // cout<<endl<<"i="<<i<<endl;
+    bool flag = false;
     for(int j=0;j<n-1;j++){
       if(swaps){
         if(arr[j]>arr[j+1]){
           swap(arr[j+1], arr[j]);
+          flag=1;
       }
     }
     else{
-      if(arr[i]<arr[j]){
+      if(arr[j]<arr[j+1]){
           swap(arr[j+1], arr[j]);
+          flag=1;
       }
     }
     // for(int i=0;i<n;i++)
     // cout<<arr[i]<<" ";
     // cout<<" "<<arr[j]<<" "<<arr[j+1];
     // cout<<endl;
+    
   }
-   
+   if(!flag)
+      break;
 }
 }
 
